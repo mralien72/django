d300b4dcd0c3a4ed274f88a4aee6c9423d62fae6
Andrey <Primer@mail.ru>
2024-06-11 16:19:00 +0500

    изменил данные для корректной работы с шаблонами html

--- myproject/myapp_hw1/views.py
+++ myproject/myapp_hw1/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import logging
-from django.template.loader import render_to_string
 
 logger = logging.getLogger(__name__)
 
 def index(request):
-    response = render_to_string('myapp_hw1/main1.html')
     logger.info('Index page accessed')
-    return HttpResponse(response)
+    return render(request, 'myapp_hw1/main.html')
 
 
 def about(request):
-    logger.info('Index page accessed')
-    response = render_to_string('myapp_hw1/data.html')
-    return HttpResponse(response)
+    logger.info('Index page accessed1')
+    return render(request, 'myapp_hw1/data.html')
