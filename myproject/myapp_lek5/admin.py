8ef85f19d8d3b707f4e69698e1844e047eccc752
Andrey <Primer@mail.ru>
2024-06-18 09:58:07 +0500

    пришел на работу, пытаюсь сделать коммит после обноления с ГИТ, выдает ошибки на 3 файла в 5 лекции

--- myproject/myapp_lek5/admin.py
+++ myproject/myapp_lek5/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
+from .admin_mixins import ExportAsCSVMixin
+
 
 @admin.action(description="Сбросить количество в ноль")
 def reset_quantity(modeladmin, request, queryset):
     queryset.update(quantity=0)
 
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin, ExportAsCSVMixin):
     """Список продуктов."""
-    list_display = ['name', 'price', 'quantity']
-    ordering = ['category', '-quantity']
+    list_display = ['name', 'price', 'quantity', 'category']
+    ordering = ['category', 'quantity']
     list_filter = ['date_added', 'price']
     search_fields = ['description']
     search_help_text = 'Поиск по полю Описание продукта'
     actions = [reset_quantity]
+    actions = ['export_as_csv']
 
     """Отдельный продукт."""
     # fields = ['name', 'description', 'category', 'date_added', 'rating']
