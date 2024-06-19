f64fe7eebebcc682d350ce4a8b3773be05f091de
Andrey <mralien72@yandex.ru>
2024-06-19 22:00:44 +0500

    Сохранение после 4 лекции v.2

--- myproject/myapp_lek4/forms.py
+++ myproject/myapp_lek4/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+import datetime
+
 
 class UserForm(forms.Form):
     name = forms.CharField(max_length=50)
     email = forms.EmailField()
     age = forms.IntegerField(min_value=0, max_value=120)
+
+
+class ManyFieldsForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    age = forms.IntegerField(min_value=18)
+    height = forms.FloatField()
+    is_active = forms.BooleanField(required=False)
+    birthdate = forms.DateField(initial=datetime.date.today)
+    gender = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')])
