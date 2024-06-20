3f15c585c1af0b180571369a6cd9dae348d41969
Andrey <Primer@mail.ru>
2024-06-20 13:53:01 +0500

    обновление ДЗ 3

--- myproject/myapp_hw3/urls.py
+++ myproject/myapp_hw3/urls.py
@@ -4,7 +4,7 @@ from .views import all_orders, orders_by_7days, orders_by_30days, orders_by_365d
 app_name = 'myapp_hw3'
 urlpatterns = [
     path('allorders/', all_orders, name='all_orders'),
-    path('7days/<int:user_id>', orders_by_7days, name='orders_by_7days'),
+    path('days/<int:user_id>', orders_by_7days, name='orders_by_7days'),
     path('30days/<int:user_id>', orders_by_30days, name='orders_by_30days'),
     path('365days/<int:user_id>', orders_by_365days, name='orders_by_365days'),
 ]
--- myproject/myapp_hw3/views.py
+++ myproject/myapp_hw3/views.py
@@ -5,16 +5,17 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def all_orders(request):
     logger.info('Index page accessed')
     orders = list(Order.objects.all())
     return render(request, 'myapp_hw3/all_orders.html', {'orders': orders})
 
 
-def orders_by_7days(request, user_id):
+def orders_by_days(request, user_id, days):
     user = get_object_or_404(User, pk=user_id)
-    last_7_days = timezone.now() - timezone.timedelta(days=7)
-    orders_user = Order.objects.filter(customer=user, date_ordered__gte=last_7_days)
+    start_date = timezone.now() - timezone.timedelta(days=days)
+    orders_user = Order.objects.filter(customer=user, date_ordered__gte=start_date)
 
     order_products = {}
     for order in orders_user:
@@ -23,61 +24,21 @@ def orders_by_7days(request, user_id):
                 order_products[product] = order.date_ordered
             else:
                 order_products[product] = max(order_products[product], order.date_ordered)
-# не понял какую дату оставлять при сортировке товара, чтобы он не повторялся,
-# по этому прописал чтобы оставался самый послений заказанный товар
 
     order_products = sorted(order_products.items(), key=lambda x: x[1], reverse=True)
-#С лямбдой у меня плохо, по этому нашел такой вариано сортировки и использовал его...
 
-    return render(request, 'myapp_hw3/orders_by_7days.html', {
+    return render(request, f'myapp_hw3/orders_by_{days}days.html', {
         'user': user,
         'order_products': order_products,
         })
+
+def orders_by_7days(request, user_id):
+    return orders_by_days(request, user_id, 7)
 
 
 def orders_by_30days(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    last_30_days = timezone.now() - timezone.timedelta(days=30)
-    orders_user = Order.objects.filter(customer=user, date_ordered__gte=last_30_days)
-
-    order_products = {}
-    for order in orders_user:
-        for product in order.products.all():
-            if product not in order_products:
-                order_products[product] = order.date_ordered
-            else:
-                order_products[product] = max(order_products[product], order.date_ordered)
-# не понял какую дату оставлять при сортировке товара, чтобы он не повторялся,
-# по этому прописал чтобы оставался самый послений заказанный товар
-
-    order_products = sorted(order_products.items(), key=lambda x: x[1], reverse=True)
-#С лямбдой у меня плохо, по этому нашел такой вариано сортировки и использовал его...
-
-    return render(request, 'myapp_hw3/orders_by_month.html', {
-        'user': user,
-        'order_products': order_products,
-        })
+    return orders_by_days(request, user_id, 30)
 
 
 def orders_by_365days(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    last_365_days = timezone.now() - timezone.timedelta(days=365)
-    orders_user = Order.objects.filter(customer=user, date_ordered__gte=last_365_days)
-
-    order_products = {}
-    for order in orders_user:
-        for product in order.products.all():
-            if product not in order_products:
-                order_products[product] = order.date_ordered
-            else:
-                order_products[product] = max(order_products[product], order.date_ordered)
-# не понял какую дату оставлять при сортировке товара, чтобы он не повторялся,
-# по этому прописал чтобы оставался самый послений заказанный товар
-
-    order_products = sorted(order_products.items(), key=lambda x: x[1], reverse=True)
-#С лямбдой у меня плохо, по этому нашел такой вариано сортировки и использовал его...
-
-    return render(request, 'myapp_hw3/orders_by_year.html', {
-        'user': user,
-        'order_products': order_products,
-        })
+    return orders_by_days(request, user_id, 365)
