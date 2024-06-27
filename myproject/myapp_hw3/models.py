ad9f2240fb3ae99c90567b1237aeb3f69654454a
Andrey <Primer@mail.ru>
2024-06-27 16:12:53 +0500

    улучшаю ДЗ 5

--- myproject/myapp_hw3/models.py
+++ myproject/myapp_hw3/models.py
@@ -4,6 +4,11 @@ from django.core.validators import MinValueValidator
 
 
 class User(models.Model):
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=12)
@@ -15,12 +20,16 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
-    quantity_products = models.IntegerField()
+    quantity_products = models.PositiveSmallIntegerField(default=0)
     date_created = models.DateField(auto_now_add=True)
     image = models.ImageField(upload_to='products/', default='products/default.jpg')
 
     def __str__(self):
         return f'Name_product: {self.name}, Price: {self.price}, Balance(Kol-vo):{self.quantity_products}'
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
 
 
 class Order(models.Model):
@@ -28,3 +37,7 @@ class Order(models.Model):
     products = models.ManyToManyField(Product)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
