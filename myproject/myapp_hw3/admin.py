71e39a3c5be50c760ae8f900789543f86d95f9df
Andrey <Primer@mail.ru>
2024-06-28 15:24:36 +0500

    Закончил ДЗ 5

--- myproject/myapp_hw3/admin.py
+++ myproject/myapp_hw3/admin.py
@@ -1,11 +1,60 @@
 from django.contrib import admin
 from .models import User, Product, Order
 
-# Register your models here.
 
-admin.site.register(User)
-admin.site.register(Product)
-admin.site.register(Order)
+@admin.action(description="Сбросить количество до нуля")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'quantity_products', 'date_created', 'image')
+    ordering = ('name', 'date_created')
+    list_filter = ('name', 'price', 'date_created')
+    search_fields = ['name', 'description']
+    search_help_text = 'Поиск по полю "Название товара" и "Описание"'
+    actions = [reset_quantity]
+    actions = ['export_as_csv']
+
+    readonly_fields = ['date_created']
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Описание',
+            {
+                'classes': ['collapse'],
+                'description': 'Описание товара',
+                'fields': ['description'],
+            },
+        ),
+        (
+            None,
+            {
+                'fields': ['price', 'quantity_products', 'image', 'date_created'],
+            },
+        )
+    ]
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address', 'date_registered')
+    ordering = ('name',)
+    list_filter = ('name', 'address', 'date_registered')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'total_price', 'date_ordered')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 
 
 
--- myproject/myapp_hw3/models.py
+++ myproject/myapp_hw3/models.py
@@ -4,11 +4,11 @@ from django.core.validators import MinValueValidator
 
 
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=12)
-    address = models.CharField(max_length=150)
-    date_registered = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    email = models.EmailField(verbose_name='Почта')
+    phone = models.CharField(max_length=12, verbose_name='Телефон')
+    address = models.CharField(max_length=150, verbose_name='Адрес')
+    date_registered = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
 
     class Meta:
         verbose_name = "Клиент"
@@ -16,15 +16,15 @@ class User(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
-    quantity_products = models.PositiveSmallIntegerField(default=0)
-    date_created = models.DateField(auto_now_add=True)
-    image = models.ImageField(upload_to='products/', default='products/default.jpg')
+    name = models.CharField(max_length=100, verbose_name='Название товара')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0)], verbose_name='Цена')
+    quantity_products = models.PositiveSmallIntegerField(default=0, verbose_name='Кол-во')
+    date_created = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    image = models.ImageField(upload_to='products/', default='products/default.jpg', verbose_name='Изображение')
 
-    def __str__(self):
-        return f'Name_product: {self.name}, Price: {self.price}, Balance(Kol-vo):{self.quantity_products}'
+    # def __str__(self):
+    #     return f'Name_product: {self.name}, Price: {self.price}, Balance(Kol-vo):{self.quantity_products}'
 
     class Meta:
         verbose_name = "Товар"
@@ -32,10 +32,10 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Номер Заказа')
     products = models.ManyToManyField(Product)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
-    date_ordered = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Сумма заказа')
+    date_ordered = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     class Meta:
         verbose_name = "Заказ"
